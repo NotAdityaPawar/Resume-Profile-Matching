d66ba555f24d8b2cb18f255fcdb16fad217d570d
Aditya Pawar <adityapawar2019@gmail.com>
2022-04-29 16:49:31 +0530

    Added something

--- resume_matching/resume_matching/settings.py
+++ resume_matching/resume_matching/settings.py
@@ -29,7 +29,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '*',
-    ''
 ]
 
 
@@ -136,8 +135,12 @@ LOGIN_REDIRECT_URL = '/jobs'
 LOGOUT_REDIRECT_URL = '/jobs'
 
 
-
-SESSION_COOKIE_SECURE = False
-CSRF_COOKIE_SECURE = False
-SECURE_SSL_REDIRECT = False
-
+CORS_REPLACE_HTTPS_REFERER      = False
+HOST_SCHEME                     = "http://"
+SECURE_PROXY_SSL_HEADER         = None
+SECURE_SSL_REDIRECT             = False
+SESSION_COOKIE_SECURE           = False
+CSRF_COOKIE_SECURE              = False
+SECURE_HSTS_SECONDS             = None
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = False
+SECURE_FRAME_DENY               = False
